6112ae6d82861d9b4836beb87dea516c3cbebddf
jarbun <jarbun306@gmail.com>
2019-09-15 23:57:50 +0530

    Remove unnecessary files

--- server.js
+++
@@ -1,6 +0,0 @@
-let express = require('express');
-let app = express();
-// serve files from the static directory
-app.use(express.static('./static'));
-
-app.listen(3000, () => console.log('Server running on PORT 3000'));
