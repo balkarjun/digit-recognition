68e952ad2e24b6ee03a8337f5dbfb531a824d19e
Arjun B <balkarjun@gmail.com>
2022-08-18 23:26:07 +0530

    add touch support

--- script.js
+++ script.js
@@ -1,85 +1,104 @@
-const canvas = document.getElementById('main-canvas');
-const smallCanvas = document.getElementById('small-canvas');
-const displayBox = document.getElementById('prediction');
+const canvas = document.getElementById("main-canvas");
+const smallCanvas = document.getElementById("small-canvas");
+const displayBox = document.getElementById("prediction");
 
-const inputBox = canvas.getContext('2d');
-const smBox = smallCanvas.getContext('2d');
+const inputBox = canvas.getContext("2d");
+const smBox = smallCanvas.getContext("2d");
 
 let isDrawing = false;
 let model;
 
 /* Loads trained model */
 async function init() {
-  model = await tf.loadModel('model/model.json');
+    model = await tf.loadModel("model/model.json");
 }
 
-canvas.addEventListener('mousedown', event => {
-  isDrawing = true;
+function drawStartEvent(event) {
+    isDrawing = true;
 
-  inputBox.strokeStyle = 'white';
-  inputBox.lineWidth = '15';
-  inputBox.lineJoin = inputBox.lineCap = 'round';
-  inputBox.beginPath();
-});
+    inputBox.strokeStyle = "white";
+    inputBox.lineWidth = "15";
+    inputBox.lineJoin = inputBox.lineCap = "round";
+    inputBox.beginPath();
+}
+canvas.addEventListener("mousedown", drawStartEvent);
+canvas.addEventListener("ontouchstart", drawStartEvent);
 
-canvas.addEventListener('mousemove', event => {
-  if (isDrawing) drawStroke(event.clientX, event.clientY);
-});
+function drawMoveEvent(event) {
+    if (isDrawing) {
+        drawStroke(event.clientX, event.clientY);
+    }
+}
+canvas.addEventListener("mousemove", drawMoveEvent);
+canvas.addEventListener("ontouchmove", drawMoveEvent);
 
-canvas.addEventListener('mouseup', event => {
-  isDrawing = false;
-  updateDisplay(predict());
-});
+function drawEndEvent(event) {
+    isDrawing = false;
+    updateDisplay(predict());
+}
+canvas.addEventListener("mouseup", drawEndEvent);
+canvas.addEventListener("ontouchend", drawEndEvent);
 
 /* Draws on canvas */
 function drawStroke(clientX, clientY) {
-  // get mouse coordinates on canvas
-  const rect = canvas.getBoundingClientRect();
-  const x = clientX - rect.left;
-  const y = clientY - rect.top;
+    // get mouse coordinates on canvas
+    const rect = canvas.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
 
-  // draw
-  inputBox.lineTo(x, y);
-  inputBox.stroke();
-  inputBox.moveTo(x, y);
+    // draw
+    inputBox.lineTo(x, y);
+    inputBox.stroke();
+    inputBox.moveTo(x, y);
 }
 
 /* Makes predictions */
 function predict() {
-  let values = getPixelData();
-  let predictions = model.predict(values).dataSync();
+    let values = getPixelData();
+    let predictions = model.predict(values).dataSync();
 
-  return predictions;
+    return predictions;
 }
 
 /* Returns pixel data from canvas after applying transformations */
 function getPixelData() {
-  smBox.drawImage(inputBox.canvas, 0, 0, smallCanvas.width, smallCanvas.height);
-  const imgData = smBox.getImageData(0, 0, smallCanvas.width, smallCanvas.height);
+    smBox.drawImage(
+        inputBox.canvas,
+        0,
+        0,
+        smallCanvas.width,
+        smallCanvas.height
+    );
+    const imgData = smBox.getImageData(
+        0,
+        0,
+        smallCanvas.width,
+        smallCanvas.height
+    );
 
-  // preserve and normalize values from red channel only
-  let values = [];
-  for (let i = 0; i < imgData.data.length; i += 4) {
-    values.push(imgData.data[i] / 255);
-  }
-  values = tf.reshape(values, [1, 28, 28, 1]);
-  return values;
+    // preserve and normalize values from red channel only
+    let values = [];
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        values.push(imgData.data[i] / 255);
+    }
+    values = tf.reshape(values, [1, 28, 28, 1]);
+    return values;
 }
 
 /* Displays predictions on screen */
 function updateDisplay(predictions) {
-  // Find index of best prediction, which corresponds to the predicted value
-  const bestPred = predictions.indexOf(Math.max(...predictions));
-  displayBox.innerText = bestPred;
+    // Find index of best prediction, which corresponds to the predicted value
+    const bestPred = predictions.indexOf(Math.max(...predictions));
+    displayBox.innerText = bestPred;
 }
 
-document.getElementById('erase').addEventListener('click', erase);
+document.getElementById("erase").addEventListener("click", erase);
 
 /* Clears canvas */
 function erase() {
-  inputBox.fillStyle = 'black';
-  inputBox.fillRect(0, 0, canvas.width, canvas.height);
-  displayBox.innerText = '';
+    inputBox.fillStyle = "black";
+    inputBox.fillRect(0, 0, canvas.width, canvas.height);
+    displayBox.innerText = "";
 }
 
 erase();
